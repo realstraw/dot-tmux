47e8ffe02f04cd7e5ca3e28b2da0443bbb7657e0
Kexin Xie <kexin.xie@salesforce.com>
2020-04-04 14:52:36 -0700

    Initial version

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+project_root="$(cd "`dirname "$0"`"; pwd)"
+
+echo "Creating .tmux directories..."
+mkdir -p ~/.tmux/plugins/
+
+echo "Linking the configurations..."
+ln $project_root/dot-tmux.conf ~/.tmux.conf
+ln $project_root/dot-tmux/dev ~/.tmux/dev
+
+echo "Installing TPM for tmux..."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
