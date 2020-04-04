c39d26ab1ea44a02cb262b4699dc053917cbb50c
Kexin Xie <kexin.xie@salesforce.com>
2020-04-04 15:29:56 -0700

    fix the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,9 @@ echo "Creating .tmux directories..."
 mkdir -p ~/.tmux/plugins/
 
 echo "Linking the configurations..."
+rm ~/.tmux.conf
 ln $project_root/dot-tmux.conf ~/.tmux.conf
+rm ~/.tmux/dev
 ln $project_root/dot-tmux/dev ~/.tmux/dev
 
 echo "Installing TPM for tmux..."
